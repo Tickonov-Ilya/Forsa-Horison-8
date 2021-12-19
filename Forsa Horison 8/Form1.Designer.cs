2a4f1a2af4ef9d83eb14c0e309225da6c0083b21
Tickonov-Ilya <ilya.tikhonov.2009@bk.ru>
2021-12-19 16:05:20 +0300

    CJ

--- Forsa Horison 8/Form1.Designer.cs
+++ Forsa Horison 8/Form1.Designer.cs
@@ -30,12 +30,279 @@ namespace Forsa_Horison_8
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblGameOver = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Coin3 = new System.Windows.Forms.PictureBox();
+            this.Enemy3 = new System.Windows.Forms.PictureBox();
+            this.Coin4 = new System.Windows.Forms.PictureBox();
+            this.Enemy2 = new System.Windows.Forms.PictureBox();
+            this.Coin1 = new System.Windows.Forms.PictureBox();
+            this.pbRoadLine4 = new System.Windows.Forms.PictureBox();
+            this.pbRoadLine3 = new System.Windows.Forms.PictureBox();
+            this.pbRoadLine2 = new System.Windows.Forms.PictureBox();
+            this.pbRoadLine1 = new System.Windows.Forms.PictureBox();
+            this.pbBoundRight = new System.Windows.Forms.PictureBox();
+            this.pbBoundLeft = new System.Windows.Forms.PictureBox();
+            this.Enemy1 = new System.Windows.Forms.PictureBox();
+            this.Car = new System.Windows.Forms.PictureBox();
+            this.Coin2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Coin3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Coin4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Coin1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRoadLine4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRoadLine3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRoadLine2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRoadLine1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBoundRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBoundLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Coin2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(37, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Coins = 0";
+            // 
+            // lblGameOver
+            // 
+            this.lblGameOver.AutoSize = true;
+            this.lblGameOver.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lblGameOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblGameOver.ForeColor = System.Drawing.Color.Red;
+            this.lblGameOver.Location = new System.Drawing.Point(183, 312);
+            this.lblGameOver.Name = "lblGameOver";
+            this.lblGameOver.Size = new System.Drawing.Size(167, 33);
+            this.lblGameOver.TabIndex = 15;
+            this.lblGameOver.Text = "Game Over";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Forsa_Horison_8.Properties.Resources.explosion;
+            this.pictureBox1.Location = new System.Drawing.Point(356, 542);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Coin3
+            // 
+            this.Coin3.Image = global::Forsa_Horison_8.Properties.Resources.png_transparent_abosolut_vodka_bottle_absolut_vodka_bottle_v_s_group_wine_vodka_barbecue_distilled_beverage_vodka_removebg_preview;
+            this.Coin3.Location = new System.Drawing.Point(203, 177);
+            this.Coin3.Name = "Coin3";
+            this.Coin3.Size = new System.Drawing.Size(28, 45);
+            this.Coin3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Coin3.TabIndex = 13;
+            this.Coin3.TabStop = false;
+            // 
+            // Enemy3
+            // 
+            this.Enemy3.Image = global::Forsa_Horison_8.Properties.Resources.vtym;
+            this.Enemy3.Location = new System.Drawing.Point(79, 58);
+            this.Enemy3.Name = "Enemy3";
+            this.Enemy3.Size = new System.Drawing.Size(100, 93);
+            this.Enemy3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Enemy3.TabIndex = 12;
+            this.Enemy3.TabStop = false;
+            // 
+            // Coin4
+            // 
+            this.Coin4.Image = global::Forsa_Horison_8.Properties.Resources.png_transparent_abosolut_vodka_bottle_absolut_vodka_bottle_v_s_group_wine_vodka_barbecue_distilled_beverage_vodka_removebg_preview;
+            this.Coin4.Location = new System.Drawing.Point(398, 89);
+            this.Coin4.Name = "Coin4";
+            this.Coin4.Size = new System.Drawing.Size(28, 50);
+            this.Coin4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Coin4.TabIndex = 11;
+            this.Coin4.TabStop = false;
+            // 
+            // Enemy2
+            // 
+            this.Enemy2.Image = global::Forsa_Horison_8.Properties.Resources.vtym;
+            this.Enemy2.Location = new System.Drawing.Point(371, 194);
+            this.Enemy2.Name = "Enemy2";
+            this.Enemy2.Size = new System.Drawing.Size(83, 88);
+            this.Enemy2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Enemy2.TabIndex = 10;
+            this.Enemy2.TabStop = false;
+            // 
+            // Coin1
+            // 
+            this.Coin1.Image = global::Forsa_Horison_8.Properties.Resources.png_transparent_abosolut_vodka_bottle_absolut_vodka_bottle_v_s_group_wine_vodka_barbecue_distilled_beverage_vodka_removebg_preview;
+            this.Coin1.Location = new System.Drawing.Point(398, 358);
+            this.Coin1.Name = "Coin1";
+            this.Coin1.Size = new System.Drawing.Size(29, 49);
+            this.Coin1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Coin1.TabIndex = 9;
+            this.Coin1.TabStop = false;
+            // 
+            // pbRoadLine4
+            // 
+            this.pbRoadLine4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pbRoadLine4.Location = new System.Drawing.Point(251, 516);
+            this.pbRoadLine4.Name = "pbRoadLine4";
+            this.pbRoadLine4.Size = new System.Drawing.Size(22, 107);
+            this.pbRoadLine4.TabIndex = 8;
+            this.pbRoadLine4.TabStop = false;
+            // 
+            // pbRoadLine3
+            // 
+            this.pbRoadLine3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pbRoadLine3.Location = new System.Drawing.Point(251, 348);
+            this.pbRoadLine3.Name = "pbRoadLine3";
+            this.pbRoadLine3.Size = new System.Drawing.Size(22, 127);
+            this.pbRoadLine3.TabIndex = 7;
+            this.pbRoadLine3.TabStop = false;
+            // 
+            // pbRoadLine2
+            // 
+            this.pbRoadLine2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pbRoadLine2.Location = new System.Drawing.Point(256, 111);
+            this.pbRoadLine2.Name = "pbRoadLine2";
+            this.pbRoadLine2.Size = new System.Drawing.Size(18, 188);
+            this.pbRoadLine2.TabIndex = 6;
+            this.pbRoadLine2.TabStop = false;
+            // 
+            // pbRoadLine1
+            // 
+            this.pbRoadLine1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pbRoadLine1.Location = new System.Drawing.Point(255, -19);
+            this.pbRoadLine1.Name = "pbRoadLine1";
+            this.pbRoadLine1.Size = new System.Drawing.Size(19, 110);
+            this.pbRoadLine1.TabIndex = 5;
+            this.pbRoadLine1.TabStop = false;
+            // 
+            // pbBoundRight
+            // 
+            this.pbBoundRight.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pbBoundRight.Location = new System.Drawing.Point(498, 12);
+            this.pbBoundRight.Name = "pbBoundRight";
+            this.pbBoundRight.Size = new System.Drawing.Size(21, 555);
+            this.pbBoundRight.TabIndex = 4;
+            this.pbBoundRight.TabStop = false;
+            // 
+            // pbBoundLeft
+            // 
+            this.pbBoundLeft.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pbBoundLeft.Location = new System.Drawing.Point(12, 12);
+            this.pbBoundLeft.Name = "pbBoundLeft";
+            this.pbBoundLeft.Size = new System.Drawing.Size(19, 555);
+            this.pbBoundLeft.TabIndex = 3;
+            this.pbBoundLeft.TabStop = false;
+            // 
+            // Enemy1
+            // 
+            this.Enemy1.Image = global::Forsa_Horison_8.Properties.Resources.vtym;
+            this.Enemy1.Location = new System.Drawing.Point(56, 399);
+            this.Enemy1.Name = "Enemy1";
+            this.Enemy1.Size = new System.Drawing.Size(100, 104);
+            this.Enemy1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Enemy1.TabIndex = 2;
+            this.Enemy1.TabStop = false;
+            // 
+            // Car
+            // 
+            this.Car.Image = ((System.Drawing.Image)(resources.GetObject("Car.Image")));
+            this.Car.Location = new System.Drawing.Point(354, 456);
+            this.Car.Name = "Car";
+            this.Car.Size = new System.Drawing.Size(102, 80);
+            this.Car.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Car.TabIndex = 1;
+            this.Car.TabStop = false;
+            this.Car.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Coin2
+            // 
+            this.Coin2.Image = global::Forsa_Horison_8.Properties.Resources.png_transparent_abosolut_vodka_bottle_absolut_vodka_bottle_v_s_group_wine_vodka_barbecue_distilled_beverage_vodka_removebg_preview;
+            this.Coin2.Location = new System.Drawing.Point(103, 271);
+            this.Coin2.Name = "Coin2";
+            this.Coin2.Size = new System.Drawing.Size(28, 50);
+            this.Coin2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Coin2.TabIndex = 0;
+            this.Coin2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(531, 579);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblGameOver);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Coin3);
+            this.Controls.Add(this.Enemy3);
+            this.Controls.Add(this.Coin4);
+            this.Controls.Add(this.Enemy2);
+            this.Controls.Add(this.Coin1);
+            this.Controls.Add(this.pbRoadLine4);
+            this.Controls.Add(this.pbRoadLine3);
+            this.Controls.Add(this.pbRoadLine2);
+            this.Controls.Add(this.pbRoadLine1);
+            this.Controls.Add(this.pbBoundRight);
+            this.Controls.Add(this.pbBoundLeft);
+            this.Controls.Add(this.Enemy1);
+            this.Controls.Add(this.Car);
+            this.Controls.Add(this.Coin2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Coin3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Coin4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Coin1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRoadLine4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRoadLine3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRoadLine2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRoadLine1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBoundRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBoundLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Coin2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Coin2;
+        private System.Windows.Forms.PictureBox Car;
+        private System.Windows.Forms.PictureBox Enemy1;
+        private System.Windows.Forms.PictureBox pbBoundLeft;
+        private System.Windows.Forms.PictureBox pbBoundRight;
+        private System.Windows.Forms.PictureBox pbRoadLine1;
+        private System.Windows.Forms.PictureBox pbRoadLine2;
+        private System.Windows.Forms.PictureBox pbRoadLine3;
+        private System.Windows.Forms.PictureBox pbRoadLine4;
+        private System.Windows.Forms.PictureBox Coin1;
+        private System.Windows.Forms.PictureBox Enemy2;
+        private System.Windows.Forms.PictureBox Coin4;
+        private System.Windows.Forms.PictureBox Enemy3;
+        private System.Windows.Forms.PictureBox Coin3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblGameOver;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer GameTimer;
     }
 }
 
--- Forsa Horison 8/Form1.cs
+++ Forsa Horison 8/Form1.cs
@@ -12,9 +12,119 @@ namespace Forsa_Horison_8
 {
     public partial class Form1 : Form
     {
+        int timerSpeed;
+        int gameSpeed;
+        int carTurnSpeed;
+        bool isGameOver;
+
+        PictureBox[] roadlines;
+        PictureBox[] enemys;
+        PictureBox[] coins;
+        Random random;
+        int collectedCoins;
+
         public Form1()
         {
             InitializeComponent();
+            init_game();
+        }
+        void init_game()
+        {
+            timerSpeed = 1000;
+            gameSpeed = 4;
+            carTurnSpeed = 15;
+            isGameOver = false;
+
+            roadlines = new PictureBox[] { pbRoadLine1, pbRoadLine2, pbRoadLine3, pbRoadLine4 };
+            enemys = new PictureBox[] { Enemy1, Enemy2, Enemy3 };
+            coins = new PictureBox[] { Coin1, Coin2, Coin3, Coin4 };
+            random = new Random();
+            collectedCoins = 0;
+
+            gen_start_pos();
+
+            Car.Image = Properties.Resources.car;
+            lblGameOver.Visible = false;
+
+            GameTimer.Interval = 10000 / timerSpeed;
+            GameTimer.Start();
+        }
+
+
+
+
+        void gen_start_pos()
+        {
+            int startY = -Enemy1.Height;
+            int x;
+            for (int i = 0; i < enemys.Length; i++)
+            {
+                x = random.Next(pbBoundLeft.Right, pbBoundRight.Left);
+                enemys[i].Location = new Point(x, startY);
+                startY -= this.Height / enemys.Length;
+            }
+        }
+
+        void move_enemys()
+        {
+            int x;
+            for (int i = 0; i < enemys.Length; i++)
+            {
+                if (enemys[i].Top > this.Height)
+                {
+                    x = random.Next(pbBoundLeft.Right, pbBoundRight.Left);
+                    enemys[i].Location = new Point(x, -enemys[i].Height);
+                }
+                else
+                {
+                    enemys[i].Top += gameSpeed;
+                }
+            }
+        }
+
+        void move_coins()
+        {
+            int x;
+            for (int i = 0; i < coins.Length; i++)
+            {
+                if (coins[i].Top > this.Height)
+                {
+                    x = random.Next(pbBoundLeft.Right, pbBoundRight.Left - coins[i].Width);
+                    coins[i].Location = new Point(x, -coins[i].Height);
+                }
+                else
+                {
+                    coins[i].Top += gameSpeed;
+                }
+            }
+        }
+
+        void move_lines()
+        {
+            int x;
+            for (int i = 0; i < roadlines.Length; i++)
+            {
+                if (roadlines[i].Top > this.Height)
+                {
+                    roadlines[i].Top = -roadlines[i].Height;
+                }
+                else
+                {
+                    roadlines[i].Top += gameSpeed;
+                }
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            move_enemys();
+            move_coins();
+            move_lines();
         }
     }
 }
